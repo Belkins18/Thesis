0fc4bd5d91d0b9b62179b93c18f1a66bd867e929
Belkins <nikolay.belibpv@gmail.com>
2015-06-25 02:47:38 +0300

    FIX: fixed 25.06.15

--- DR/src/dr/CriteriaForm.java
+++ DR/src/dr/CriteriaForm.java
@@ -8,6 +8,7 @@ import dr.calculate.secondtEtap.HLCriteria;
 import dr.calculate.secondtEtap.HWCriteria;
 import dr.calculate.secondtEtap.MMCriteria;
 import dr.calculate.secondtEtap.MinusMatrix;
+import dr.calculate.secondtEtap.MultCriteria;
 import dr.calculate.secondtEtap.SCriteria;
 import dr.criteria.BLCriteriaForm;
 import dr.criteria.GCriteriaForm;
@@ -36,6 +37,7 @@ public class CriteriaForm extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -105,6 +107,16 @@ public class CriteriaForm extends javax.swing.JFrame {
             }
         });
 
+        jButton8.setBackground(new java.awt.Color(51, 255, 51));
+        jButton8.setFont(new java.awt.Font("GOST type A", 1, 18)); // NOI18N
+        jButton8.setForeground(new java.awt.Color(0, 0, 0));
+        jButton8.setText("П Критерій");
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -122,11 +134,12 @@ public class CriteriaForm extends javax.swing.JFrame {
                             .addComponent(jButton3)
                             .addComponent(jButton5)
                             .addComponent(jButton6)
-                            .addComponent(jButton7))
+                            .addComponent(jButton7)
+                            .addComponent(jButton8))
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
 
-        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton2, jButton3, jButton5, jButton6, jButton7});
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton2, jButton3, jButton5, jButton6, jButton7, jButton8});
 
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -145,10 +158,12 @@ public class CriteriaForm extends javax.swing.JFrame {
                 .addComponent(jButton6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton7)
-                .addContainerGap(107, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton8)
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
-        jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jButton2, jButton3, jButton5, jButton6, jButton7});
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jButton2, jButton3, jButton5, jButton6, jButton7, jButton8});
 
         jButton4.setBackground(new java.awt.Color(0, 102, 102));
         jButton4.setFont(new java.awt.Font("GOST type A", 1, 18)); // NOI18N
@@ -222,7 +237,7 @@ public class CriteriaForm extends javax.swing.JFrame {
         dr.calculate.firstEtap.TranstormMatrix tm = new TranstormMatrix();
         tm.setTM(ts.getZO());
         tm.PrintResult();
-       System.out.println("//=== S Criteria ===" + "\n");
+        System.out.println("//=== S Criteria ===" + "\n");
         SCriteria sCriteria = new SCriteria();
         MinusMatrix minusMatrix = new MinusMatrix();
         minusMatrix.setMinusMatrix(tm.getTM());
@@ -303,6 +318,26 @@ public class CriteriaForm extends javax.swing.JFrame {
         gCriteriaForm.setVisible(true);
     }//GEN-LAST:event_jButton7ActionPerformed
 
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        dr.calculate.firstEtap.ThirdStep ts = new ThirdStep();
+        System.out.println("//==============================================================================" + "\n");
+        dr.calculate.firstEtap.TranstormMatrix tm = new TranstormMatrix();
+        tm.setTM(ts.getZO());
+        tm.PrintResult();
+        MinusMatrix minusMatrix = new MinusMatrix();
+        minusMatrix.setMinusMatrix(tm.getTM());
+        System.out.println("//=== П Criteria ===" + "\n");
+        MultCriteria multCriteria = new MultCriteria();
+        multCriteria.setA(minusMatrix.getMinusMatrix());
+//         multCriteria.setA(tm.getTM());
+//         multCriteria.getA();
+        multCriteria.setMatrix(minusMatrix.getMinusMatrix(), multCriteria.getA());
+//         multCriteria.getMatrix();
+        multCriteria.setMult(multCriteria.getMatrix());
+        multCriteria.getMult();
+        System.out.println(Arrays.toString(multCriteria.getMult()));
+    }//GEN-LAST:event_jButton8ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -347,6 +382,7 @@ public class CriteriaForm extends javax.swing.JFrame {
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
